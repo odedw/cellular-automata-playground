bf04600ee38c14b94b10ccea94d2e0cc43f2e4ff
Oded Welgreen <oded.welgreen@gmail.com>
2017-09-07 16:20:17 +0300

    switch to webgl

--- src/Renderer.js
+++ src/Renderer.js
@@ -1,68 +1,78 @@
 export default class Renderer {
-  constructor(canvas, cols, rows) {
-    canvas.width *= 10;
-    canvas.height *= 10;
-    const context = canvas.getContext('2d'),
-      cellWidth = Math.ceil(canvas.width / cols),
-      cellHeight = Math.ceil(canvas.height / rows);
+  constructor(cols, rows, cellWidth, cellHeight) {
+    const renderer = PIXI.autoDetectRenderer(256, 256, {
+      antialias: false,
+      transparent: false,
+      resolution: 1
+    });
+    renderer.view.style.position = 'absolute';
+    renderer.view.style.display = 'block';
+    renderer.autoResize = true;
+    renderer.resize(window.innerWidth, window.innerHeight);
+    renderer.backgroundColor = 0x000000;
+    document.body.appendChild(renderer.view);
+    const stage = new PIXI.Container();
+    const graphics = new PIXI.Graphics();
+    stage.addChild(graphics);
+    renderer.render(stage);
+
     let mouseDown = false;
 
-    const onDraw = event => {
-      if (!mouseDown) return;
+    // const onDraw = event => {
+    //   if (!mouseDown) return;
 
-      const rect = canvas.getBoundingClientRect();
-      const mousePos = {
-        x: event.clientX * canvas.width / canvas.clientWidth,
-        y: event.clientY * canvas.height / canvas.clientHeight
-      };
-      const pos = {
-        i: ~~(mousePos.y / cellHeight),
-        j: ~~(mousePos.x / cellWidth)
-      };
+    //   const rect = canvas.getBoundingClientRect();
+    //   const mousePos = {
+    //     x: event.clientX * canvas.width / canvas.clientWidth,
+    //     y: event.clientY * canvas.height / canvas.clientHeight
+    //   };
+    //   const pos = {
+    //     i: ~~(mousePos.y / cellHeight),
+    //     j: ~~(mousePos.x / cellWidth)
+    //   };
 
-      this.onDraw(pos.i, pos.j);
-    };
+    //   this.onDraw(pos.i, pos.j);
+    // };
 
-    canvas.addEventListener('mousedown', evt => {
-      mouseDown = true;
-      onDraw(evt);
-    });
+    // canvas.addEventListener('mousedown', evt => {
+    //   mouseDown = true;
+    //   onDraw(evt);
+    // });
 
-    canvas.addEventListener('mousemove', onDraw);
-    canvas.addEventListener('mouseup', evt => (mouseDown = false));
-
-    const colorForCell = (state, neighbours) => {
-      return state === 1 ? 'white' : '#282828';
-    };
+    // canvas.addEventListener('mousemove', onDraw);
+    // canvas.addEventListener('mouseup', evt => (mouseDown = false));
 
     this.render = (world, diff) => {
-      context.clearRect(0, 0, canvas.width, canvas.height);
-      context.fillStyle = 'white';
-      const deadCells = [];
-      context.beginPath();
-      context.fillStyle = 'rgba(255, 255, 255, 255)';
-      diff
-        .filter(cell => cell.nextState === 1)
-        .forEach(cell =>
-          context.fillRect(
-            cell.j * cellWidth,
-            cell.i * cellHeight,
-            cellWidth,
-            cellHeight
-          )
-        );
-      context.fillStyle = 'rgba(40, 40, 40, 255)';
-      diff
-        .filter(cell => cell.nextState !== 1)
-        .forEach(cell =>
-          context.fillRect(
-            cell.j * cellWidth,
-            cell.i * cellHeight,
-            cellWidth,
-            cellHeight
-          )
-        );
-      context.closePath();
+      // context.clearRect(0, 0, canvas.width, canvas.height);
+      graphics.beginFill(0xffffff);
+      // context.beginPath();
+      // context.fillStyle = 'rgba(255, 255, 255, 255)';
+      diff.filter(cell => cell.nextState === 1).forEach(cell =>
+        graphics.drawRect(
+          //     context.fillRect(
+          cell.j * cellWidth,
+          cell.i * cellHeight,
+          cellWidth,
+          cellHeight
+        )
+      );
+      graphics.endFill();
+      graphics.beginFill(0x000000);
+      // context.fillStyle = 'rgba(40, 40, 40, 255)';
+      diff.filter(cell => cell.nextState !== 1).forEach(cell =>
+        graphics.drawRect(
+          // context.fillRect(
+          cell.j * cellWidth,
+          cell.i * cellHeight,
+          cellWidth,
+          cellHeight
+        )
+      );
+
+      graphics.endFill();
+      renderer.render(stage);
+
+      // context.closePath();
     };
   }
 }
--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 import Engine from './Engine';
 import Renderer from './Renderer';
 import Stats from '../lib/stats.min';
+// import PIXI from '../lib/pixi.min';
 
 const stats = new Stats();
 stats.showPanel(0);
 document.body.appendChild(stats.dom);
 
 const pixelsPerCell = 3,
-  canvas = document.getElementById('canvas'),
-  cols = Math.ceil(canvas.clientWidth / pixelsPerCell),
-  rows = Math.ceil(canvas.clientHeight / pixelsPerCell),
-  renderer = new Renderer(canvas, cols, rows),
+  cols = Math.ceil(window.innerWidth / pixelsPerCell),
+  rows = Math.ceil(window.innerHeight / pixelsPerCell),
+  renderer = new Renderer(cols, rows, pixelsPerCell, pixelsPerCell),
   engine = new Engine(
     cols, //number of columns
     rows, //number of rows
