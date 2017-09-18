e65b8d738b10a7317d69583dcc47dfe37112c789
Oded Welgreen <oded.welgreen@gmail.com>
2017-09-18 23:25:16 +0300

    precompute neighbour indices

--- src/World.js
+++ src/World.js
@@ -2,7 +2,7 @@ export default class World {
   constructor(rows, cols, randomStart) {
     const arr = new Uint8Array(new ArrayBuffer(rows * cols)),
       index = (i, j) => i * cols + j;
-
+    const neighboursIndices = new Array(cols * rows);
     this.get = (i, j) =>
       i >= 0 && i < rows && j >= 0 && j < cols ? arr[index(i, j)] : undefined;
 
@@ -16,15 +16,27 @@ export default class World {
       if (i + 1 < rows) arr[index(i + 1, j)] = 1;
     };
 
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < cols; j++) {
+        neighboursIndices[index(i, j)] = [];
+        if (i - 1 >= 0 && j - 1 >= 0)
+          neighboursIndices[index(i, j)].push(index(i - 1, j - 1));
+        if (i - 1 >= 0) neighboursIndices[index(i, j)].push(index(i - 1, j));
+        if (i - 1 >= 0 && j + 1 < cols)
+          neighboursIndices[index(i, j)].push(index(i - 1, j + 1));
+
+        if (j - 1 >= 0) neighboursIndices[index(i, j)].push(index(i, j - 1));
+        if (j + 1 < cols) neighboursIndices[index(i, j)].push(index(i, j + 1));
+
+        if (i + 1 < rows && j - 1 >= 0)
+          neighboursIndices[index(i, j)].push(index(i + 1, j - 1));
+        if (i + 1 < rows) neighboursIndices[index(i, j)].push(index(i + 1, j));
+        if (i + 1 < rows && j + 1 < cols)
+          neighboursIndices[index(i, j)].push(index(i + 1, j + 1));
+      }
+    }
     this.neighbours = (i, j) =>
-      (this.get(i - 1, j - 1) || 0) +
-      (this.get(i - 1, j) || 0) +
-      (this.get(i - 1, j + 1) || 0) +
-      (this.get(i, j - 1) || 0) +
-      (this.get(i, j + 1) || 0) +
-      (this.get(i + 1, j - 1) || 0) +
-      (this.get(i + 1, j) || 0) +
-      (this.get(i + 1, j + 1) || 0);
+      neighboursIndices[index(i, j)].reduce((a, b) => a + arr[b], 0);
 
     if (randomStart)
       for (let i = 0; i < arr.length; i++) arr[i] = Math.round(Math.random());
